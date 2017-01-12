5212986403a576f34806daf59e8e397cf614c380
Edward Gao <egao@sent.com>
2017-01-11 22:17:16 -1000

    routes work

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	console.log('getting articles');
+	res.write('NO');
+	res.end();
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser);
+const articles = require('./routes/articles');
+const products = require('./routes/products');
+
+app.use(bodyParser.json());
+app.use('/articles', articles);
+app.use('/products', products);
+
+app.get('/', (req, res) => {
+	res.write('heyy!!!');
+	res.end();
+});
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
