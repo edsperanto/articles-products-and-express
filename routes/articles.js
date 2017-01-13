c011fca59d05bcc6c68429f630777b6c1e8a9eeb
Edward Gao <egao@sent.com>
2017-01-12 18:45:37 -1000

    product checks if price is number

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	console.log('getting articles');
-	res.write('NO');
-	res.end();
+router.post('/', (req, res) => {
+
+});
+
+router.put('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const router = express.Router();
+let productData = {
+	newId: 1
+};
+
+router.post('/', (req, res) => {
+	function priceToNumber() {
+		let priceStr = req.body.price.trim();
+		priceStr = (priceStr.charAt(0) === '$') ? (priceStr.substr(1)) : (priceStr);
+		return isNaN(Number(priceStr)) ? (false) : (Number(priceStr));
+	}
+	function productHasValidFormat() {
+		let nameIsStr = typeof req.body.name === 'string';
+		let priceIsNum = typeof priceToNumber() === 'number';
+		let inventoryIsStr = typeof req.body.inventory === 'string';
+		return nameIsStr && priceIsNum && inventoryIsStr;
+	}
+	if(productHasValidFormat()) {
+		console.log('yes');
+		let currentIndex = productData.newId;
+		productData[currentIndex] = req.body;
+		productData[currentIndex]["id"] = currentIndex;
+		productData.newId++;
+		res.end();
+	}
+});
+
+router.put('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+
+});
 
 module.exports = router;
