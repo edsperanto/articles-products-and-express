277002fe36e49dffeb13664c0384c8f5263d713c
Edward Gao <egao@sent.com>
2017-01-17 20:35:35 -1000

    add test for PUT request

--- test/counter-spec.js
+++ test/counter-spec.js
@@ -10,6 +10,11 @@ let my = {
 			"name": "lemonade",
 			"price": "$1.25",
 			"inventory": "100"
+		},
+		newLemonade: {
+			"id": "",
+			"price": "$3.5",
+			"inventory": "10"
 		}
 	}
 };
@@ -78,6 +83,38 @@ describe('Products API', () => {
 						newProduct.name.should.equal(lemonade.name);
 						newProduct.price.should.equal(lemonade.price);
 						newProduct.inventory.should.equal(lemonade.inventory);
+						my.product.newLemonade.id = newProduct.id;
+					}
+					done();
+				});
+		});
+	});
+
+	describe('PUT /api/products', () => {
+		it('should update old product', done => {
+			let newLemonade = my.product.newLemonade;
+			agent.put(`/api/products/${newLemonade.id}`)
+				.set('Content-Type', 'application/x-www-form-urlencoded')
+				.send(newLemonade)
+				.expect('Content-Type', /json/)
+				.expect(200)
+				.end((err, res) => {
+					let editProduct;
+					if(err) throw err;
+					res.body.should.have.property('success');
+					res.body.success.should.be.a('Boolean');
+					res.body.success.should.be.true;
+					res.body.should.have.property('edited product');
+					if(res.body['new product'] !== undefined) {
+						editProduct = res.body['edited product'];
+						editProduct.should.be.a('Object');
+						editProduct.should.have.property('id');
+						editProduct.should.have.property('name');
+						editProduct.should.have.property('price');
+						editProduct.should.have.property('inventory');
+						editProduct.name.should.equal(newLemonade.name);
+						editProduct.price.should.equal(newLemonade.price);
+						editProduct.inventory.should.equal(newLemonade.inventory);
 					}
 					done();
 				});
